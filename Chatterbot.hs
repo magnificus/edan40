887341efea38c1bee4c27a2669b9b06464d2e3bd
unknown <tobiaselinder@gmail.com>
2015-11-19 00:43:03 +0100

    init

---
+++ Chatterbot.hs
@@ -0,0 +1,162 @@
+module Chatterbot where
+import Utilities
+import System.Random
+import Data.Char
+
+chatterbot :: String -> [(String, [String])] -> IO ()
+chatterbot botName botRules = do
+    putStrLn ("\n\nHi! I am " ++ botName ++ ". How are you?")
+    botloop
+  where
+    brain = rulesCompile botRules
+    botloop = do
+      putStr "\n: "
+      question <- getLine
+      answer <- stateOfMind brain
+      putStrLn (botName ++ ": " ++ (present . answer . prepare) question)
+      if (not . endOfDialog) question then botloop else return ()
+
+--------------------------------------------------------
+
+type Phrase = [String]
+type PhrasePair = (Phrase, Phrase)
+type BotBrain = [(Phrase, [Phrase])]
+
+
+--------------------------------------------------------
+
+stateOfMind :: BotBrain -> IO (Phrase -> Phrase)
+{- TO BE WRITTEN -}
+stateOfMind _ = return id
+
+rulesApply :: [PhrasePair] -> Phrase -> Phrase
+{- TO BE WRITTEN -}
+rulesApply _ = id
+
+reflect :: Phrase -> Phrase
+{- TO BE WRITTEN -}
+reflect = id
+
+reflections =
+  [ ("am",     "are"),
+    ("was",    "were"),
+    ("i",      "you"),
+    ("i'm",    "you are"),
+    ("i'd",    "you would"),
+    ("i've",   "you have"),
+    ("i'll",   "you will"),
+    ("my",     "your"),
+    ("me",     "you"),
+    ("are",    "am"),
+    ("you're", "i am"),
+    ("you've", "i have"),
+    ("you'll", "i will"),
+    ("your",   "my"),
+    ("yours",  "mine"),
+    ("you",    "me")
+  ]
+
+
+---------------------------------------------------------------------------------
+
+endOfDialog :: String -> Bool
+endOfDialog = (=="quit") . map toLower
+
+present :: Phrase -> String
+present = unwords
+
+prepare :: String -> Phrase
+prepare = reduce . words . map toLower . filter (not . flip elem ".,:;*!#%&|") 
+
+rulesCompile :: [(String, [String])] -> BotBrain
+{- TO BE WRITTEN -}
+rulesCompile _ = []
+
+
+--------------------------------------
+
+
+reductions :: [PhrasePair]
+reductions = (map.map2) (words, words)
+  [ ( "please *", "*" ),
+    ( "can you *", "*" ),
+    ( "could you *", "*" ),
+    ( "tell me if you are *", "are you *" ),
+    ( "tell me who * is", "who is *" ),
+    ( "tell me what * is", "what is *" ),
+    ( "do you know who * is", "who is *" ),
+    ( "do you know what * is", "what is *" ),
+    ( "are you very *", "are you *" ),
+    ( "i am very *", "i am *" ),
+    ( "hi *", "hello *")
+  ]
+
+reduce :: Phrase -> Phrase
+reduce = reductionsApply reductions
+
+reductionsApply :: [PhrasePair] -> Phrase -> Phrase
+{- TO BE WRITTEN -}
+reductionsApply _ = id
+
+
+-------------------------------------------------------
+-- Match and substitute
+--------------------------------------------------------
+
+-- Replaces a wildcard in a list with the list given as the third argument
+substitute :: Eq a => a -> [a] -> [a] -> [a]
+substitute _ [] _ = []
+substitute a (x:xs) bs
+	| x == a = bs ++ substitute a xs bs
+	| otherwise = x : substitute a xs bs
+
+
+-- Tries to match two lists. If they match, the result consists of the sublist
+-- bound to the wildcard in the pattern list.
+match :: Eq a => a -> [a] -> [a] -> Maybe [a]
+match _ [] [] = Just []
+match _ _ [] = Nothing
+match _ [] _ = Nothing	
+match wc (p:ps) (s:ss)
+  | p == wc = orElse (singleWildcardMatch (wc:ps) (s:ss)) (longerWildcardMatch (wc:ps) (s:ss))
+  | p == s  = match wc ps ss
+  | otherwise = Nothing
+
+
+-- Helper function to match
+singleWildcardMatch, longerWildcardMatch :: Eq a => [a] -> [a] -> Maybe [a]
+longerWildcardMatch (wc:ps) (s:ss) = mmap (s:) (match wc (wc:ps) ss)
+singleWildcardMatch (wc:ps) (s:ss) = match wc ps ss >> return [s]
+
+
+
+-- Test cases --------------------
+
+testPattern =  "a=*;"
+testSubstitutions = "32"
+testString = "a=32;"
+
+substituteTest = substitute '*' testPattern testSubstitutions
+substituteCheck = substituteTest == testString
+
+matchTest = match '*' testPattern testString
+matchCheck = matchTest == Just testSubstitutions
+
+
+
+-------------------------------------------------------
+-- Applying patterns
+--------------------------------------------------------
+
+-- Applying a single pattern
+transformationApply :: Eq a => a -> ([a] -> [a]) -> [a] -> ([a], [a]) -> Maybe [a]
+transformationApply wc f s map = mmap (substitute wc (snd map)) (mmap f (match wc (fst map) s))
+
+frenchPresentations = [("My name is ", "Je m'appelle "), ("FUCK ", "Tits ")]
+
+-- Applying a list of patterns until one succeeds
+transformationsApply :: Eq a => a -> ([a] -> [a]) -> [([a], [a])] -> [a] -> Maybe [a]
+transformationsApply wc f [] t = Nothing
+transformationsApply wc f (p:ps) l 
+	|	transformationApply wc f l p == Nothing = transformationsApply wc f ps l
+	|	otherwise = transformationApply wc f l p
